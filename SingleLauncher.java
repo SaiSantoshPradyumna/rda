8e071ae7d96ee2e42409d2721e45fabc6fbe48c4
SaiSantoshPradyumna <santoshpradyumna@gmail.com>
2025-03-01 23:05:24 +0530

    ds

--- SingleLauncher.java
+++ SingleLauncher.java
@@ -1,15 +1,5 @@
-import java.awt.BorderLayout;
-import java.awt.Color;
-import java.awt.Dimension;
-import java.awt.EventQueue;
-import java.awt.FlowLayout;
-import java.awt.GridBagConstraints;
-import java.awt.GridBagLayout;
-import java.awt.Insets;
-import java.awt.MouseInfo;
-import java.awt.Rectangle;
-import java.awt.Robot;
-import java.awt.Toolkit;
+import java.awt.*;
+
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 import java.awt.event.HierarchyEvent;
